0e750d8373c1bc6115432bf4bd099dde509534db
angletank <1169218442@qq.com>
2020-12-10 09:06:12 +0800

    创建项目，启动项目

---
+++ src/main/java/com/xxxx/crm/Starter.java
@@ -0,0 +1,15 @@
+package com.xxxx.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author wangjie
+ * @Date 2020/12/9 20:20
+ */
+@SpringBootApplication
+public class Starter {
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class,args);
+    }
+}
